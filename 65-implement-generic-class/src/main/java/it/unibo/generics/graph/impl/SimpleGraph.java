f52dc1a8764c9ca46f56b66561886c5fed3703ca
Nicolas Tazzieri <nicolas.tazzieri@studio.unibo.it>
2024-11-02 15:59:58 +0100

    Enhanced Ex. 65

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/SimpleGraph.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/SimpleGraph.java
@@ -3,9 +3,7 @@ package it.unibo.generics.graph.impl;
 import java.util.List;
 import java.util.Set;
 import java.util.Map;
-import java.util.Queue;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -51,7 +49,7 @@ public class SimpleGraph<N> implements Graph<N> {
      * {@inheritDoc}
      */
     @Override
-    public Set<N> linkedNodes(N node) {
+    public Set<N> linkedNodes(final N node) {
         return new HashSet<>(graph.get(node));
     }
 
@@ -59,35 +57,57 @@ public class SimpleGraph<N> implements Graph<N> {
      * {@inheritDoc}
      */
     @Override
-    public List<N> getPath(N source, N target) {
+    public List<N> getPath(final N source, final N target) {
         if(!graph.containsKey(source) || !graph.containsKey(target) ){
             return new ArrayList<>();
         }
-        final Set<List<N>> frontier = new HashSet<>();
-        final List<N> initList = new ArrayList<>(); 
-        initList.add(source);                   // Adding source path
-        frontier.add(initList);
-        N currentNode = source;                 
-        while(!frontier.isEmpty()) {            // Moving the frontier until it's empty
+        final Set<List<N>> frontier = initFrontier(source);
+        //Moving the frontier until it's empty
+        while(!frontier.isEmpty()) {            
             final Iterator<List<N>> pathIterator = frontier.iterator();
-            final Set<List<N>> newPaths = new HashSet<>();
-            while(pathIterator.hasNext()) {     // Creating paths until target is found or all precedent paths were examinated 
-                final List<N> path = pathIterator.next();
-                currentNode = path.getLast();
-                for (N node : graph.get(currentNode)) {
-                    if(!path.contains(node)) {
-                        final List<N> newPath = new ArrayList<>(path);
-                        newPath.addLast(node);
-                        if(node.equals(target))  {
-                            return newPath;
-                        }
-                        newPaths.add(newPath);
-                    }
-                }
+            Set<List<N>> newPaths = new HashSet<>();
+            // Creating new paths until all precedent paths were examinated (and removed)
+            while(pathIterator.hasNext()) {    
+                moveFrontier(newPaths, pathIterator.next());
                 pathIterator.remove();
+            }
+            // Checking if the target was found during frontier expansion
+            for(final List<N> path : newPaths) {
+                if(path.getLast().equals(target)) {
+                    return path;
+                }
             }
             frontier.addAll(newPaths);
         }
         return new ArrayList<>();
+    }
+
+    /**
+     * Given a path, adds in newPaths the new paths found 
+     * @param newPaths set of new paths 
+     * @param path source path
+     */
+    private void moveFrontier(final Set<List<N>> newPaths, final List<N> path) {
+        final N currentNode = path.getLast();
+        for (final N node : graph.get(currentNode)) {
+            if(!path.contains(node)) {
+                final List<N> newPath = new ArrayList<>(path);
+                newPath.addLast(node);
+                newPaths.add(newPath);
+            }
+        }
+    }
+
+    /**
+     * Initializes frontier
+     * @param source Source node to add to the frontier
+     * @return initialized set
+     */
+    private Set<List<N>> initFrontier(final N source) {
+        final Set<List<N>> frontier = new HashSet<>();
+        final List<N> initList = new ArrayList<>(); 
+        initList.add(source);                   // Adding source path
+        frontier.add(initList);
+        return frontier;
     }
 }
